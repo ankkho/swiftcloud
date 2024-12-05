d0566451afcf8ed87a59105f350dce9fbc5734a6
ankkho <1430541+ankkho@users.noreply.github.com>
2024-12-05 18:23:16 +0800

    chore: resolved graphql load issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
-	imports: [
-		GraphQLModule.forRoot<ApolloDriverConfig>({
-			driver: ApolloDriver,
-			playground: false,
-		}),
-	],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      typePaths: ['./**/*.graphql'],
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+  ],
 })
 export class AppModule {}
