9d75984c4e1fbd12f0cc04f88ca70b3fb4768c06
ankkho <1430541+ankkho@users.noreply.github.com>
2024-12-05 18:28:41 +0800

    chore: added data

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<string> {
-	const app = await NestFactory.create(AppModule);
-	app.useGlobalPipes(new ValidationPipe());
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
 
-	await app.listen(3000);
-	return app.getUrl();
+  await app.listen(3000);
+  return app.getUrl();
 }
 
 bootstrap()
-	.then((url) => {
-		console.log(`Application is running on: ${url}`);
-	})
-	.catch((error: unknown) => {
-		console.error(error);
-	});
+  .then((url) => {
+    console.log(`Application is running on: ${url}`);
+  })
+  .catch((error: unknown) => {
+    console.error(error);
+  });
